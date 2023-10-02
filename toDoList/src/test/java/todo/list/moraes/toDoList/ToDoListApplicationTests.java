6ad8c8a485f78919544ab5c300fb89ebf20b8aab
guilhermemoraessiqueira <guimoraessiqueira26@gmail.com>
2023-10-02 17:56:43 -0300

    Primeiro commit

---
+++ toDoList/src/test/java/todo/list/moraes/toDoList/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package todo.list.moraes.toDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
